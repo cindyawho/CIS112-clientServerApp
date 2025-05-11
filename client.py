88c8c3a69dd8e6cc6ef1432aa288c73500609cb3
Cindy Andrade <cindya3939@gmail.com>
2025-05-11 13:55:59 -0700

    Send and receive simple message between server and client

--- client.py
+++ client.py
@@ -7,6 +7,14 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
 client.connect(server_address)
 
-print(f'Connected to host at port 12345')
+print(f'~Connected to host at port {server_address[1]}')
 
-# run this python client.py only after running server.py
+# Note: run this python client.py only after running server.py
+
+
+## sending and receiving data
+client.send('Hello server! How is the weather today?'.encode())
+received = client.recv(1024)
+print(f'~~Server Responded: {received}')
+client.send('Thank you! Goodbye'.encode())
+client.close()
--- server.py
+++ server.py
@@ -7,13 +7,26 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP, capital bc it'
 # takes single input as a tuple
 server.bind(('localhost', 12345)) 
 
-server.listen() # no argument means no queue specified
+server.listen(5)
 
 print('Server is Live and Listening on Port: 12345!')
 
 client, caddress = server.accept()
-print(f'Connection from {caddress} opened...')
+print(f'~Connection from {caddress} opened...')
 
 # RUN THIS CODE FIRST on terminal: 
 # python server.py 
 # then run client.py
+
+# Sending and receiving data with server
+## sending and receiving data
+received = client.recv(1024)
+print(f'~~Client Responded: {received}')
+client.send('Hello Client. Thanks for joining us. The weather today is delightful'.encode())
+
+received = client.recv(1024)
+print(f'~~~Client Responded: {received}')
+
+# Close connection with client and the overall server
+client.close()
+server.close()
